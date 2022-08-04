4e91a9ad30afa381dab1f3bc7bb4c1a291fc5d83
Richard Mattinson <richard.m@ttinson.net>
2022-08-04 16:12:59 +0100

    get counter

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(express.json());
 
 let counter = 0;
 
+app.get("/counter", (request, response) => {
+    console.log("COUNTER, COME IN!");
+    // response.send("That counts")
+    response.json({ counter });
+});
+
 const port = 3030;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}/`);
